e66efc031816a66d555dc8e8447b64975fd460a0
Selçuk Öztürk <selcukahmed@gmail.com>
2026-03-02 21:07:27 +0200

    Remove accidentally committed garth_temp and add to gitignore

--- garth_temp/garth/stats/training_status/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    "DailyTrainingStatus",
-    "MonthlyTrainingStatus",
-    "WeeklyTrainingStatus",
-]
-
-from .daily import DailyTrainingStatus
-from .monthly import MonthlyTrainingStatus
-from .weekly import WeeklyTrainingStatus
